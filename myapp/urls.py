b173cf413c39eab308ff085b0cb1b4d4767ec0cc
glmzhn <galymzhan2007@gmail.com>
2024-10-24 07:36:11 +0500

    Second

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet, NotificationViewSet
-
+from .views import TaskViewSet
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
-router.register(r'notifications', NotificationViewSet)
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
